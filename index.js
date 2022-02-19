afae80aad4c3bda08eaa77b6e9a86872c3397893
Jay-vaghani <89519011+Jay-vaghani@users.noreply.github.com>
2022-02-19 14:02:57 +0530

    finel

---
+++ index.js
@@ -0,0 +1,54 @@
+let buttons = document.querySelectorAll(".drum")
+
+for(let i = 0 ; i < buttons.length ; i++){
+    buttons[i].addEventListener("click", function(){
+        let buttonInnerHTML = this.innerHTML
+        
+        makeSound(buttonInnerHTML)
+
+        buttonAnimation(buttonInnerHTML)
+    })
+}
+
+document.addEventListener("keypress", function(event){
+    makeSound(event.key)
+
+    buttonAnimation(event.key)
+})
+
+function makeSound(key){
+    switch(key){
+        case "w":
+                new Audio("sounds/tom-1.mp3").play()
+            break;
+        case "a":
+                new Audio("sounds/tom-2.mp3").play()
+            break;
+        case "s":
+                new Audio("sounds/tom-3.mp3").play()
+            break;
+        case "d":
+                new Audio("sounds/tom-4.mp3").play()
+            break;
+        case "j":
+                new Audio("sounds/snare.mp3").play()
+            break;
+        case "k":
+                new Audio("sounds/crash.mp3").play()
+            break;
+        case "l":
+                new Audio("sounds/kick-bass.mp3").play()
+            break;
+        default:
+    }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey)
+
+    activeButton.classList.add("pressed")
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    }, 100)
+}
